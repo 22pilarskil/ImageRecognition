75e9919594086fe4b19b517b41ed2b206bdd1c34
22pilarskil <49341850+22pilarskil@users.noreply.github.com>
2019-06-27 18:35:42 -0600

    Added regularization and early stop

--- vectorizedNetwork.py
+++ vectorizedNetwork.py
@@ -4,31 +4,6 @@ import codecs
 import matplotlib.pyplot as plt
 import json
 
-class crossEntropyCost(object):
-	@staticmethod
-	def fn(a, y):
-		return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
-	@staticmethod
-	def delta(z, a, y):
-		return (a.transpose()-y)
-
-def toInt(b):
-	return int(codecs.encode(b, "hex"), 16)
-
-def normalize(rawArray, range_):
-	array = np.copy(rawArray).astype(np.float32)
-	if range_ == (0, 1):
-		return range_
-	array-=range_[0]
-	dist = abs(range_[0])+abs(range_[1])
-	array /= dist
-	return array
-
-def vectorize(num):
-	array = np.zeros(10)
-	array[num] = 1
-	return array
-
 def displayImage(pixels, label = None):
   figure = plt.gcf()
   figure.canvas.set_window_title("Number display")
@@ -37,39 +12,55 @@ def displayImage(pixels, label = None):
   plt.imshow(pixels, cmap = "gray")
   plt.show()
 
-def loadFile(fileName, mode="rb"):
-	with open(fileName, mode) as raw:
-		data = raw.read()
-		magicNumber = toInt(data[:4])
-		length = toInt(data[4:8])
-		if magicNumber==2049:
-			#labels
-			parsed = np.frombuffer(data, dtype=np.uint8, offset = 8)
-		elif magicNumber==2051:
-			#images
-			numOfRows = toInt(data[8:12])
-			numOfColumns = toInt(data[12:16])
-			parsed = normalize(np.frombuffer(data, dtype=np.uint8, offset = 16).reshape(length, numOfRows*numOfColumns), (0, 255))
-		else: return -1
-		return parsed
-
-def loadData(vector=True):
+def loadData():
 	print("Loading Data...")
+	def toInt(b):
+		return int(codecs.encode(b, "hex"), 16)
+
+	def normalize(rawArray, range_):
+		array = np.copy(rawArray).astype(np.float32)
+		if range_ == (0, 1):
+			return range_
+		array-=range_[0]
+		dist = abs(range_[0])+abs(range_[1])
+		array /= dist
+		return array
+
+	def vectorize(num):
+		array = np.zeros(10)
+		array[num] = 1
+		return array
+
+	def loadFile(fileName, mode="rb"):
+		with open(fileName, mode) as raw:
+			data = raw.read()
+			magicNumber = toInt(data[:4])
+			length = toInt(data[4:8])
+			if magicNumber==2049:
+				#labels
+				parsed = np.frombuffer(data, dtype=np.uint8, offset = 8)
+			elif magicNumber==2051:
+				#images
+				numOfRows = toInt(data[8:12])
+				numOfColumns = toInt(data[12:16])
+				parsed = normalize(np.frombuffer(data, dtype=np.uint8, offset = 16).reshape(length, numOfRows*numOfColumns), (0, 255))
+			else: return -1
+			return parsed
+	#add validation if needed
+	data = {"train":[], "test":[]}
+	trainImages = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/train-images-idx3-ubyte")
+	trainLabels = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/train-labels-idx1-ubyte")
+	data["train"] = np.asarray(list(zip(trainImages, np.asarray([vectorize(i) for i in trainLabels]))))
+	testLabels = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/t10k-labels-idx1-ubyte")
+	testImages = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/t10k-images-idx3-ubyte")
+	data["test"] = np.asarray(list(zip(testImages, np.asarray([vectorize(i) for i in testLabels]))))
+	return data
+
 	'''
 	Before using this function, you must first download all four of the binary array files from this link (http://yann.lecun.com/exdb/mnist/)
 	Drag them to your directory without double clicking them, and only once you have placed them in a separate directory (in my case, 'data') do you double click them
 	Change the argument in the laodFile function to the directory paths of each of the files
 	'''
-	data = {"train":[], "validation":[], "test":[]}
-	trainImages = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/train-images-idx3-ubyte")
-	trainLabels = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/train-labels-idx1-ubyte")
-	if vector:
-		data["train"] = np.asarray(list(zip(trainImages, np.asarray([vectorize(i) for i in trainLabels]))))
-	else: data["train"] = np.asarray(list(zip(trainImages, np.asarray([i for i in trainLabels]))))
-	testLabels = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/t10k-labels-idx1-ubyte")
-	testImages = loadFile("/Users/MichaelPilarski1/Desktop/Neural_Network/data/t10k-images-idx3-ubyte")
-	data["test"] = np.asarray(list(zip(testImages, np.asarray([vectorize(i) for i in testLabels]))))
-	return data
 
 def sigmoid(x):
 	return float(1)/(float(1)+np.exp(-x))
@@ -103,7 +94,7 @@ def retreiveNetwork():
 
 class Network():
 
-	def __init__ (self, sizes, trainedWeights=None, trainedBiases=None, saveNetworkStuff=True, cost=crossEntropyCost):
+	def __init__ (self, sizes, trainedWeights=None, trainedBiases=None, saveNetworkStuff=True):
 		self.numOfLayers = len(sizes)
 		if (trainedWeights==None):
 			self.weights = [np.random.randn(y, x) for y, x in zip(sizes[1:], sizes[:-1])]
@@ -112,7 +103,8 @@ class Network():
 			self.weights = trainedWeights
 			self.biases = trainedBiases
 		self.saveNetworkStuff = saveNetworkStuff
-		self.cost = cost
+		self.minimum = 100
+		self.streak = 0
 
 	def saveNetwork(self):
 		trainedBiases = {}
@@ -128,29 +120,43 @@ class Network():
 		with open("biases.txt", 'w+') as JSONFile:
 			json.dump(trainedBiases, JSONFile)
 
-	def SGD(self, trainingData, miniBatchSize, epochs, eta, testData):
+	def earlyStop(self, totalPercent, epochs):
+		specialNum = .05*epochs+4
+		if (totalPercent>self.minimum): self.streak+=1
+		else: 
+			self.minimum = totalPercent
+			self.streak = 0
+		if (self.streak>=specialNum): return True
+		else: return False
+
+	def SGD(self, trainingData, miniBatchSize, epochs, eta, testData, regularizationParam=False):
 		print("Starting Stochastic Gradient Descent...")
+		lmbda = 0
+		if (regularizationParam): lmbda = regularizationParam
 		printNumber = 1
 		for j in range(epochs):
 			random.shuffle(trainingData)
 			miniBatches = [trainingData[k:k+miniBatchSize] for k in range(0, len(trainingData), miniBatchSize)]
 			for miniBatch in miniBatches:
-				self.updateMiniBatch(miniBatch, eta)
+				self.updateMiniBatch(miniBatch, eta, len(trainingData), lmbda)
 			totalCorrect = 0
-			totalPercentx = 0
+			totalPercent = 0
 			numOfTests = len(testData)
 			for x, y in testData:
-				totalPercent, correct = self.mse(x.reshape(784, 1), y)
+				percent, correct = self.mse(x.reshape(784, 1), y)
 				totalCorrect+=correct
-				totalPercentx+=totalPercent
-			totalPercentx/=numOfTests
+				totalPercent+=percent
+			totalPercent/=(numOfTests/100)
 			if (j%printNumber==0):
-				print("Epoch %d complete. Percent Error: %.8f. Total Correct: %d/%d" %(j, totalPercentx*100, totalCorrect, numOfTests))
+				print("Epoch %d complete. Percent Error: %.8f. Total Correct: %d/%d" %(j, totalPercent, totalCorrect, numOfTests))
+			if (self.earlyStop(totalPercent, epochs)):
+				print("Network oversaturated- exiting SGD")
+				break
 		if(self.saveNetworkStuff):
 			self.saveNetwork()
 			print("Weights and Biases Saved")
 
-	def updateMiniBatch(self, miniBatch, eta):
+	def updateMiniBatch(self, miniBatch, eta, n, lmbda):
 		weightError = [np.zeros(w.shape) for w in self.weights]
 		biasError = [np.zeros(b.shape) for b in self.biases]
 		for x, y in miniBatch:
@@ -158,8 +164,7 @@ class Network():
 			deltaWeightError, deltaBiasError = self.backprop(x, y)
 			weightError = [we+dwe for we, dwe in zip(weightError, deltaWeightError)]
 			biasError = [be+dbe for be, dbe in zip(biasError, deltaBiasError)]
-		#try changing eta/len to just eta
-		self.weights = [w-(((float(eta)/len(miniBatch))*we)) for w, we in zip(self.weights, weightError)]
+		self.weights = [(1-eta*(lmbda/n))*w-(((float(eta)/len(miniBatch))*we)) for w, we in zip(self.weights, weightError)]
 		self.biases = [b-(((float(eta)/len(miniBatch))*be)) for b, be in zip(self.biases, biasError)]
 
 	def backprop(self, x, y):
@@ -175,7 +180,7 @@ class Network():
 			zs.append(z)
 			activation = sigmoid(z)
 			activations.append(activation)
-		delta[-1] = (self.cost).delta(zs[-1], activations[-1], y)
+		delta[-1] = self.costDerivative(activations[-1], y)
 		weightError[-1] = np.dot(activations[-2], delta[-1]).T
 		biasError[-1] = delta[-1].T
 		for i in range(2, self.numOfLayers):
@@ -192,21 +197,20 @@ class Network():
 	def mse(self, x, y):
 		prediction = self.feedforward(x).reshape(10)
 		correct = 0
-		totalPercent = (np.linalg.norm(y.reshape(1, 10)-prediction) ** 2) 
+		percent = (np.linalg.norm(y.reshape(1, 10)-prediction) ** 2) 
 		if np.argmax(prediction) == np.argmax(y): correct = 1
-		return totalPercent, correct
+		return percent, correct
 
 	def classify(self, trainingData):
 		x = self.feedforward(trainingData[0].reshape(784, 1)).reshape(1, 10)
 		print("Network prediction: %d. Actual: %d" %(np.argmax(x), trainingData[1]))
 		displayImage(trainingData[0].reshape(28, 28))
 		
-def costDerivative(y, activation):
-	whatINeed = y-activation
-	return whatINeed
+	def costDerivative(self, activation, y):
+		return activation.T-y
 #Parameter Declarations-------------
 numOfInputs = 784
-epochs = 4
+epochs = 20
 sizeOfMinis = 10
 learnRate = 2
 sizes = np.array([numOfInputs,30,10])
